3fc2987991701f22902f1bc226fd136bd512573d
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2018-07-24 15:37:15 +0100

    Remove silly comment

--- test-module.js
+++ test-module.js
@@ -8,9 +8,6 @@ describe('testing range module:', ()=>{
     assert.deepEqual(range(1, 10), [1,2,3,4,5,6,7,8,9,10]);
   });
 
-  /*
-   * coś tu źle
-   */
   it('range(1, -3) will return null', ()=>{
     assert.strictEqual(range(1, -3), null);
   });
