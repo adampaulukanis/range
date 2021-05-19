5e195cf2154844cdcae4e5a31f51938d5fefef59
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-05-19 21:36:17 +0200

    Add new test
    Also reformat the source code
    And fix typo: then -< than

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-'use strict';
+"use strict";
 
 module.exports = function range(start, end, step = 1) {
-  if (start == end) {
-    return [start];
-  }
-
-  let tab = [];
-  if (start < end) {
-    for (let i = start; i <= end; i += step) {
-      tab.push(i);
+    if (start == end) {
+        return [start];
     }
-  }
-  else {
-    // e.g.: range(3, -3)
-    if (step > 0)
-      step *= -1;
 
-    for (let i = start; i >= end; i += step) {
-      tab.push(i);
+    let tab = [];
+    if (start < end) {
+        for (let i = start; i <= end; i += step) {
+            tab.push(i);
+        }
+    } else {
+        // e.g.: range(3, -3)
+        if (step > 0) step *= -1;
+
+        for (let i = start; i >= end; i += step) {
+            tab.push(i);
+        }
     }
-  }
-  return tab;
-}
+    return tab;
+};
--- test.js
+++ test.js
@@ -1,30 +1,38 @@
-'use strict';
+"use strict";
 
-const range = require('.');
-const assert = require('assert');
+const range = require(".");
+const assert = require("assert");
 
-describe('check range', () => {
-  it('range(1, 10) should produce [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
-    assert.deepEqual(range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-  });
+describe("check range", () => {
+    it("range(1, 10) should produce [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+        assert.deepEqual(range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
 
-  it('range(1, 10) should be the same as range(1, 10, 1)', () => {
-    assert.deepEqual(range(1, 10), range(1, 10, 1));
-  });
+    it("range(1, 10) should be the same as range(1, 10, 1)", () => {
+        assert.deepEqual(range(1, 10), range(1, 10, 1));
+    });
 
-  it('range(1, 10, 2) should return [1, 3, 5, 7, 9]', () => {
-    assert.deepEqual(range(1, 10, 2), [1, 3, 5, 7, 9]);
-  });
+    it("range(1, 10, 2) should return [1, 3, 5, 7, 9]", () => {
+        assert.deepEqual(range(1, 10, 2), [1, 3, 5, 7, 9]);
+    });
 
-  it('range(1, 1) should return [1]', () => {
-    assert.deepEqual(range(1, 1), [1]);
-  });
+    it("range(1, 1) should return [1]", () => {
+        assert.deepEqual(range(1, 1), [1]);
+    });
 
-  it('range(5, 2, -1) produces [5, 4, 3, 2]', () => {
-    assert.deepEqual(range(5, 2, -1), [5, 4, 3, 2]);
-  });
+    it("range(5, 2, -1) produces [5, 4, 3, 2]", () => {
+        assert.deepEqual(range(5, 2, -1), [5, 4, 3, 2]);
+    });
 
-  it('if start < end then step should be -1 even if not provided: range(3, -3)', () => {
-    assert.deepEqual(range(3, -3), [3, 2, 1, 0, -1, -2, -3]);
-  });
+    it("range(5, 2) produces [5, 4, 3, 2]", () => {
+        assert.deepEqual(range(5, 2), [5, 4, 3, 2]);
+    });
+
+    it("range(5, 2, 1) produces [5, 4, 3, 2]", () => {
+        assert.deepEqual(range(5, 2, 1), [5, 4, 3, 2]);
+    });
+
+    it("if start < end than step should be -1 even if not provided: range(3, -3)", () => {
+        assert.deepEqual(range(3, -3), [3, 2, 1, 0, -1, -2, -3]);
+    });
 });
